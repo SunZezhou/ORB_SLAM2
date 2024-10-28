21ac516550fd2a7c33b0135ae27561a16133a5a9
Zezhou Sun <szz905262953@gmail.com>
2024-10-28 17:27:57 +0400

    agri

---
+++ Examples/Monocular/rgb.py
@@ -0,0 +1,9 @@
+import os
+
+file_names = os.listdir("/home/zezhousun/Downloads/agribot_data/rgb")
+file_names.sort(key=lambda x: int(x[:-4]))
+
+f= open("/home/zezhousun/Downloads/agribot_data/rgb.txt", 'w')
+for name in file_names:
+    f.write(name[0:10]+'.'+name[11:-4]+" rgb/"+name+"\n")
+f.close()
